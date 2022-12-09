1fecb5289032f55046f30cc4d1726ad106b53a96
Harsh Patil <harshnilkanth2015@gmail.com>
2022-12-08 18:50:32 -0700

    Landing Page
    - Basic landing page format implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,22 @@
-import  Navbar  from './components/layout/Navbar/Navbar';
+import  Navbar  from './components/layout/navbar/navbar';
 import  Landing  from './components/pages/Landing/Landing';
 import  LogIn  from './components/pages/LogIn/LogIn';
 import  SignUp  from './components/pages/SignUp/SignUp';
 import  Feed  from './components/pages/Feed/Feed';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import PrivateRoutes from './components/routing/PrivateRoutes';
-
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
         <div className='content'>
           <Routes>
             <Route exact path='/' element={<Landing />} />
             <Route exact path='/logIn' element={<LogIn />} />
             <Route exact path='/signUp' element={<SignUp />} />
             <Route exact path='/feed' element={<Feed />} />
-
           </Routes>
         </div>
       </Router>
--- frontend/src/components/pages/Landing/Landing.jsx
+++ frontend/src/components/pages/Landing/Landing.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+import './Landing.css'
 
 
 const Landing = () => {
 
     return(
-        <div>landing</div>
+        <>
+            <Navbar bg="primary" variant="light">
+                <Container>
+                <Navbar.Brand href="#home">UniTea</Navbar.Brand>
+                <Nav>
+                    <Nav.Link href="#login">
+                        <Button variant="outlined-primary">Login</Button>
+                    </Nav.Link>
+                    <Nav.Link href="#sign-up">
+                        <Button variant="secondary">Sign Up</Button>
+                    </Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
+            <body>
+            <div className='landing-text'>
+                Stand out and reach other students with UniTEA's student focused social media app
+            </div>
+            </body>
+            
+        </>
     )
 }
 export default Landing
