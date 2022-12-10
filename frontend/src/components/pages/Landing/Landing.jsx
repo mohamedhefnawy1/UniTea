abf67f69354136957e152d8beed0a8e26f5d45d1
Harsh Patil <harshnilkanth2015@gmail.com>
2022-12-10 13:50:58 -0700

    Removed Landing.css

--- frontend/src/components/pages/Landing/Landing.jsx
+++ frontend/src/components/pages/Landing/Landing.jsx
@@ -4,8 +4,6 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 
-import './Landing.css'
-
 
 const Landing = () => {
 
