8e5633001f2a36314c7cf20b8dfd4ec948307614
mohamedhefnawy1 <mohamedhefnawy10@gmail.com>
2022-12-11 16:15:31 -0700

    adding create post box

---
+++ frontend/src/components/layout/create_post/Create_post.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+
+
+const Create_post = () => {
+        
+
+    return (
+        <div className="Create_post">
+            <div className="Create_post_box">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Create_post
