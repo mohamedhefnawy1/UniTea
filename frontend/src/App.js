245f294d8d87070be738ec1a4860e9f563e9356a
mohamedhefnawy1 <mohamedhefnawy10@gmail.com>
2022-12-11 14:09:14 -0700

    fixing merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
+import { useEffect } from 'react'
 import  Navbar  from './components/layout/navbar/navbar';
 import  Landing  from './components/pages/Landing/Landing';
 import  LogIn  from './components/pages/LogIn/LogIn';
 import  SignUp  from './components/pages/SignUp/SignUp';
 import  Feed  from './components/pages/Feed/Feed';
-import Post_feed from './components/layout/post_feed/Post_feed';
-import Sidebar from './components/layout/Sidebar/Sidebar';
+import Profile from './components/pages/Profile/[id]';
+import Notify from './components/notify/Notify'
 
 
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { refreshToken } from './redux/actions/authAction';
 
 import PrivateRoutes from './components/routing/PrivateRoutes';
 
@@ -16,17 +19,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const { auth } = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(refreshToken())
+  }, [dispatch])
+
   return (
     <div className="App">
       <Router>
+      <Notify/>
       <Navbar />
         <div className='content'>
           <Routes>
-            <Route exact path='/' element={<Landing />} />
-            <Route exact path='/logIn' element={<LogIn />} />
+            <Route exact path='/' element={auth.token ? <Landing/> : <LogIn/>} />
+            <Route exact path='/landing' element={<Landing />} />
             <Route exact path='/signUp' element={<SignUp />} />
             <Route exact path='/feed' element={<Feed />} />
-
+            <Route exact path='/profile:id' element={<Profile />} />
+          
           </Routes>
         </div>
       </Router>
