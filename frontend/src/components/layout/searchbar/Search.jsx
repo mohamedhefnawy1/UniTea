d803a317a176c26d4cdae0a497f9a15ccd2c126b
Mahmoud <mahmoud.abdelaty@ucalgary.ca>
2022-12-10 19:51:04 -0700

    Update Search.jsx

--- frontend/src/components/layout/searchbar/Search.jsx
+++ frontend/src/components/layout/searchbar/Search.jsx
@@ -3,59 +3,16 @@ import styled, { css } from "styled-components";
 
 function Index(props) {
   return (
-    <Button>
-      <ButtonOverlay>
-        <ImageStack>
-          <Image src={require("../../../assets/search.png")}></Image>
-          <Placeholder placeholder=""></Placeholder>
-        </ImageStack>
-      </ButtonOverlay>
-    </Button>
+    <div className='buttonMan'>
+      <button className='buttonDawg'>
+        <div>
+          <img className='imgP' src={require("./assets/images/image_iWP8..png")}></img>
+          <input className="placeholderINPUT" placeholder=""></input>
+        </div>
+      </button>
+    </div>
   );
 }
-const Button = styled.div`
-  width: 282px;
-  height: 39px;
-  background-color: #E6E6E6;
-  flex-direction: column;
-  display: flex;
-  border: none;
-`;
 
-const ButtonOverlay = styled.button`
- display: block;
- background: none;
- height: 100%;
- width: 100%;
- border:none
- `;
-const Image = styled.img`
-  position: absolute;
-  top: 6px;
-  left: 241px;
-  height: 28px;
-  width: 27px;
-  object-fit: contain;
-`;
-
-const Placeholder = styled.input`
-  font-family: Roboto;
-  top: 0px;
-  left: 0px;
-  position: absolute;
-  font-style: normal;
-  font-weight: 400;
-  color: #121212;
-  height: 39px;
-  width: 282px;
-  border: none;
-  background: transparent;
-`;
-
-const ImageStack = styled.div`
-  width: 282px;
-  height: 39px;
-  position: relative;
-`;
 
 export default Index;
