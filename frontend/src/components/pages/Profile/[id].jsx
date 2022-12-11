d5cd65402336c5b65331ae3d17658b521a35e725
mohamedhefnawy1 <mohamedhefnawy10@gmail.com>
2022-12-11 15:05:06 -0700

    adding profile pic to navbar

--- frontend/src/components/pages/Profile/[id].jsx
+++ frontend/src/components/pages/Profile/[id].jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Info from './Info'
 import Posts from './Posts'
 import './Profile.css'
-
+import Navbar from '../../layout/navbar/Navbar';
 
 export const Profile = () => {
   return (
     <div className="profile">
+        <Navbar />
+
         <Info/>
         <br />
         <hr />
